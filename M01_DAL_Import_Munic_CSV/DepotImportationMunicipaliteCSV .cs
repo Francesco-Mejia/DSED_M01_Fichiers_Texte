f9cad434de4ab41e5a152eb075566f425292232e
Francesco-Mejia <2391510@csfoy.ca>
2024-10-16 00:13:38 -0400

    Creation de la classe DepotImportationMunicipaliteCSV qui implemente IDepotImportationMunicipalite

---
+++ M01_DAL_Import_Munic_CSV/DepotImportationMunicipaliteCSV .cs
@@ -0,0 +1,19 @@
+﻿using M01_Entite;
+
+namespace M01_DAL_Import_Munic_CSV
+{
+    public class DepotImportationMunicipaliteCSV : IDepotImportationMunicipalite
+    {
+        private readonly string _cheminFichier;
+
+        public DepotImportationMunicipaliteCSV(string cheminFichier)
+        {
+            this._cheminFichier = cheminFichier;
+        }
+
+        public IEnumerable<Municipalite> LireMunicipalite()
+        {
+            
+        }
+    }
+}
