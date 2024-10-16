c2c12fbd65c13754b0be2ed38c453d45301d506e
Francesco-Mejia <2391510@csfoy.ca>
2024-10-16 01:44:53 -0400

    Appel des depots et statistiques dans le main

--- DSED_M01_Fichiers_Texte/Program.cs
+++ DSED_M01_Fichiers_Texte/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace DSED_M01_Fichiers_Texte
+﻿using M01_DAL_Import_Munic_CSV;
+using M01_DAL_Municipalite_SQLServer;
+using M01_Srv_Municipalite;
+
+namespace DSED_M01_Fichiers_Texte
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            var depotImportation = new DepotImportationMunicipaliteCSV("\"C:\\CSFOY AUTOMNE 2024\\Developpement_de_services_echange_donnees\\Exercices\\MUN.csv\"");
+
+            var depotMunicipalites = new DepotMunicipalitesSQLServer();
+
+            var traitement = new TraitementImporterDonneesMunicipalite(depotImportation, depotMunicipalites);
+
+            var statistiques = traitement.Executer();
+
+            Console.WriteLine("Statistiques d'importation:");
+            Console.WriteLine(statistiques.ToString());
         }
     }
 }
