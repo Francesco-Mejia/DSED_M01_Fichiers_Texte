e52a7f18eabb5a0435d6a53a7db40ec5a602df21
Francesco-Mejia <2391510@csfoy.ca>
2024-10-16 01:38:58 -0400

    Implementation de l'interface IDepotMunicipalites dans la classe DepotMunicipalitesSQLServer

---
+++ M01_DAL_Municipalite_SQLServer/DepotMunicipalitesSQLServer .cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M01_Entite;
+
+namespace M01_DAL_Municipalite_SQLServer
+{
+    public class DepotMunicipalitesSQLServer : IDepotMunicipalites
+    {
+        private readonly MunicipaliteContext context;
+
+        public DepotMunicipalitesSQLServer()
+        {
+            this.context = new MunicipaliteContext();
+        }
+
+        public Municipalite chercherMunicipaliteParCodeGeographique(int codeGeographique)
+        {
+            return context.Municipalites
+                .FirstOrDefault(m => m.CodeGeographique == codeGeographique);
+        }
+
+        public IEnumerable<Municipalite> listerMunicipalitesActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DesactiverMunicipalite(Municipalite municipalite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AjouterMunicipalite(Municipalite municipalite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MAJMunicipalite(Municipalite municipalite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
