c4718bfd8a7921001026e331ffe11a3a3262df21
Francesco-Mejia <2391510@csfoy.ca>
2024-10-16 00:30:26 -0400

    classe DepotImportationMunicipaliteCSV COMPLETE

--- M01_DAL_Import_Munic_CSV/DepotImportationMunicipaliteCSV .cs
+++ M01_DAL_Import_Munic_CSV/DepotImportationMunicipaliteCSV .cs
@@ -13,7 +13,33 @@ namespace M01_DAL_Import_Munic_CSV
 
         public IEnumerable<Municipalite> LireMunicipalite()
         {
-            
+            var municipalites = new List<Municipalite>();
+
+            using (var reader = new StreamReader(_cheminFichier))
+            {
+                reader.ReadLine();
+
+                string ligne;
+                while ((ligne = reader.ReadLine()) != null)
+                {
+                    string[] champs = ligne.Split(',');
+                    if (champs.Length >= 7)
+                    {
+                        municipalites.Add(new Municipalite
+                        {
+                            CodeGeographique = int.Parse(champs[0]),
+                            Nom = champs[1],
+                            AdresseCourriel = champs[7],
+                            AdresseWeb = champs[8],
+                            DateConstruction = champs[18],
+                            Superficie = double.Parse(champs[21]),
+                            Population = int.Parse(champs[22])
+                        });
+                    }
+                }
+            }
+
+            return municipalites;
         }
     }
 }
