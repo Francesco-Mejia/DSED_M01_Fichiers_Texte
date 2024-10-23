f92f75dfd23454c183b542cf8c13a2b69bfa8af4
Francesco-Mejia <2391510@csfoy.ca>
2024-10-22 20:25:40 -0400

    Modifications dans le fichier sql

--- M01_SQL/BD_Municipalites.sql
+++ M01_SQL/BD_Municipalites.sql
@@ -6,28 +6,15 @@ GO
 
 CREATE TABLE municipalites 
 (
-    MunicipaliteId INT IDENTITY(1,1) PRIMARY KEY,
-    CodeGeographique INT NOT NULL,
-    Nom NVARCHAR(255) NOT NULL,
-    AdresseCourriel NVARCHAR(255),
-    AdresseWeb NVARCHAR(255),
-    DateConstruction DATE,
-    Superficie FLOAT,
-    Population INT,
+    mcode INT PRIMARY KEY,
+    munnom NVARCHAR(255) NOT NULL,
+    mcourriel NVARCHAR(255),
+    mweb NVARCHAR(255),
+    mdatcons DATE,
+    msuperf FLOAT,
+    mpopul INT,
     Actif BIT NOT NULL DEFAULT 1
 );
-GO
-
-EXEC sp_rename 'municipalites.CodeGeographique', 'mcode', 'COLUMN';
-EXEC sp_rename 'municipalites.Nom', 'munnom', 'COLUMN';
-EXEC sp_rename 'municipalites.AdresseCourriel', 'mcourriel', 'COLUMN';
-EXEC sp_rename 'municipalites.AdresseWeb', 'mweb', 'COLUMN';
-EXEC sp_rename 'municipalites.DateConstruction', 'mdatcons', 'COLUMN';
-EXEC sp_rename 'municipalites.Population', 'mpopul', 'COLUMN';
-EXEC sp_rename 'municipalites.Superficie', 'msuperf', 'COLUMN';
-ALTER TABLE municipalites DROP COLUMN MunicipaliteId;
-ALTER TABLE municipalites
-ADD CONSTRAINT PK_municipalites PRIMARY KEY (mcode);
 GO
 
 SELECT * FROM municipalites;
