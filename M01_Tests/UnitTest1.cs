b87315bf1c160c28d4e9039a4abef368ee207466
Francesco-Mejia <2391510@csfoy.ca>
2024-10-22 20:49:43 -0400

    Ajout du projet tests et setup du fichier avec les packages demandés

---
+++ M01_Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Moq;
+using M01_DAL_Municipalite_SQLServer;
+using M01_Srv_Municipalite;
+using M01_Entite;
+using System.Collections.Generic;
+
+namespace M01_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
