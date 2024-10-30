62ecf1a54c935651a54a74199b97d8f5bb3f3d4b
Francesco-Mejia <2391510@csfoy.ca>
2024-10-30 00:55:01 -0400

    Creation du DTO Municipalite

---
+++ M01_Entite/MunicipaliteDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01_Entite
+{
+    public class MunicipaliteDTO
+    {
+        public int mcode { get; set; }
+        public string munnom { get; set; }
+        public string? mcourriel { get; set; }
+        public string? mweb { get; set; }
+        public DateTime? mdatcons { get; set; }
+        public decimal? msuperf { get; set; }
+        public int? mpopul { get; set; }
+        public bool Actif { get; set; } = true;
+    }
+}
